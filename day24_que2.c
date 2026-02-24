39ae9f9fbce5f469e5bfaa9875f9db295f9f228a
Chahat Sangwan <sangwanchahat990@gmail.com>
2026-02-24 13:32:47 +0530

    Add files via upload

---
+++ day24_que2.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node* insertEnd(struct Node* head, int value) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = value;
+    newNode->next = NULL;
+
+    if (head == NULL)
+        return newNode;
+
+    struct Node* temp = head;
+    while (temp->next != NULL)
+        temp = temp->next;
+
+    temp->next = newNode;
+    return head;
+}
+
+struct Node* removeElements(struct Node* head, int val) {
+    while (head != NULL && head->data == val) {
+        struct Node* temp = head;
+        head = head->next;
+        free(temp);
+    }
+
+    struct Node* current = head;
+    while (current != NULL && current->next != NULL) {
+        if (current->next->data == val) {
+            struct Node* temp = current->next;
+            current->next = temp->next;
+            free(temp);
+        } else {
+            current = current->next;
+        }
+    }
+
+    return head;
+}
+
+void display(struct Node* head) {
+    struct Node* temp = head;
+    while (temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+}
+
+int main() {
+    int n, value, val;
+    struct Node* head = NULL;
+
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &value);
+        head = insertEnd(head, value);
+    }
+
+    scanf("%d", &val);
+
+    head = removeElements(head, val);
+
+    display(head);
+
+    return 0;
+}
